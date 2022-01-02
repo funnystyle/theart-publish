16fc1436e28fd8ec1080a45f83b1dac9821a77f6
Funnystyle <esero@hanmail.net>
2022-01-03 00:41:27 +0900

    20211229

--- contents/js/design.js
+++ contents/js/design.js
@@ -74,4 +74,56 @@ $(document).ready(function(){
 		$(this).addClass("on");
 	});
 	/* //tab */
+
+	/* top 버튼 */
+	$(".btn_top").on("click", function(){
+		$("body, html").animate({
+			scrollTop : 0
+		}, 300);
+	});
+	/* //top 버튼 */
+
+	/* 상세페이지 */
+	$(".btn_exhibit").on("click", function(e){
+		e.preventDefault();
+
+		$(".floating_layout_area").addClass("show");
+	});
+	/* //상세페이지 */
+
+
+	$(window).on('scroll', function() {
+		$(".simulation_area").each(function() {
+			if($(window).scrollTop() >= $(this).offset().top - 500) {
+				$(".f_floating_area").addClass("show");
+			}
+			else if($(window).scrollTop() == 0){
+				$(".f_floating_area").removeClass("show");
+			}
+		});
+	});
+
+	/* sort select */
+	$(".btn_sort").on("click", function(){
+		$(this).toggleClass("show");
+	});
+	/* //sort select */
+
+	/* layer pop */
+	$(".close_layer, .layer_bg, .close_layer_review").on("click", function(e){
+		e.preventDefault();
+
+		$(".layer_pop_area").removeClass("open");
+		$(".wrap").removeClass("layer_open");
+	});
+
+	$(".layer_open_link").on("click", function(e){
+		e.preventDefault();
+
+		var layer_select = $(this).attr("href");
+
+		$(layer_select).addClass("open");
+		$(".wrap").addClass("layer_open");
+	});
+	/* //layer pop */
 });
